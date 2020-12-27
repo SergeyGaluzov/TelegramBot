4858ace32f0bb7b65d2740683654fe481e311397
Sergey Galuzov <seregasuper2000@gmail.com>
2020-12-27 11:13:09 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
