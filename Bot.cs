aae4b93f5c6f8a1dd4537e6c5947f84c69717499
Obruch Yaroslav <Stamin4ik4@gmail.com>
2020-12-27 14:05:11 +0200

    init bot settings

---
+++ Bot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TelegramBot
+{
+    class Bot
+    {
+        public static string Key { get; set; } = "1443954908:AAHCZAiDcDr1oFxawDSbHfUIX1yYTvCqWJ4";
+        public static string Name { get; set; } = "pivo_game_bot"; 
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
