85f3bc7cf5589b22044cdbe5fec141dc0e9a05ca
Sergey Galuzov <seregasuper2000@gmail.com>
2020-12-27 14:02:10 +0200

    add gameParams

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
 ﻿using System;
+using System.Web.Mvc;
+
 namespace TelegramBot
 {
+    public class GameParameters
+    {
+        public string Topic {get; set; }
+        public string Difficulty { get; set; }
+        public int QuestionsNumber { get; set; }
+    }
+    
+    [Route("game/millionaire")]
+    public class MillionereController : Controller
+    {
+        [HttpGet]
+        public GameParameters Get(string topic, string difficulty, int questionsNumber)
+        {
+            var gameParams = new GameParameters();
+            gameParams.Topic = topic;
+            gameParams.Difficulty = difficulty;
+            gameParams.QuestionsNumber = questionsNumber;
+            return gameParams;
+        }   
+    }
     class Program
     {
         static void Main(string[] args)
